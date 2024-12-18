971a5998e72be1dba7e02a81ff0612770729f3a5
Jacek programmer <jacek301@gmail.com>
2024-12-19 00:34:06 +0100

    reorganising tabel cell. add test image. add bootstrap

--- src/app/habit-board/habit-board.component.ts
+++ src/app/habit-board/habit-board.component.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatTableModule } from '@angular/material/table';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCardModule } from '@angular/material/card';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 export interface Month {
   january: number;
@@ -12,14 +16,15 @@ export interface Month {
 }
 
 const ELEMENT_DATA: Month[] = [
-  {january: 1, february: 1, march: 1, april: 1, may : "1 nie ma"},
-  {january: 2, february: 2, march: 0, april: 2 , may : "2 V" },  
-  {january: 2, february: 2, march: 0, april: 2 , may : "3 V" }  
+  { january: 1, february: 1, march: 1, april: 1, may: "1 nie ma" },
+  { january: 2, february: 2, march: 0, april: 2, may: "2 V" },
+  { january: 3, february: 2, march: 0, april: 2, may: "3 V" }
 ];
 
 @Component({
   selector: 'app-habit-board',
-  imports: [MatSlideToggleModule, MatTableModule, MatButtonModule],
+  imports: [MatSlideToggleModule, MatTableModule, MatButtonModule,
+     MatSelectModule, MatCardModule, MatChipsModule, MatFormFieldModule],
   templateUrl: './habit-board.component.html',
   styleUrl: './habit-board.component.css'
 })
