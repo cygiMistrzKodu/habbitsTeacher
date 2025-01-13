7ce01bff46d6a79400cafb68862e3badcd060b92
Jacek programmer <jacek301@gmail.com>
2025-01-13 02:09:57 +0100

    improve options level chose

--- src/app/habit-board/habit-board-cell/habit-board-cell.component.ts
+++ src/app/habit-board/habit-board-cell/habit-board-cell.component.ts
@@ -1,20 +1,31 @@
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
 import { MatCardModule } from '@angular/material/card';
-import {MatChipsModule} from '@angular/material/chips';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-habit-board-cell',
   imports: [MatSlideToggleModule, MatTableModule, MatButtonModule,
-    MatSelectModule, MatCardModule, MatChipsModule, MatFormFieldModule],
+    MatSelectModule, MatCardModule, MatChipsModule, MatFormFieldModule, CommonModule],
   templateUrl: './habit-board-cell.component.html',
   styleUrl: './habit-board-cell.component.css'
 })
 export class HabitBoardCellComponent {
-  @Input() dayOfMonth: any = '' ;
+  @Input() dayOfMonth: any = '';
+
+  selectedLevel: string = ""
+
+  levels = [
+    { value: 'notDone', viewValue: 'not done' },
+    { value: 'mini', viewValue: 'mini' },
+    { value: 'medium', viewValue: 'medium' },
+    { value: 'elite', viewValue: 'elite' },
+
+  ]
 
 }
