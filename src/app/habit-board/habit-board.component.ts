aa521928a48278e3cd023a4993cbe22ca017ae87
Jacek programmer <jacek301@gmail.com>
2025-01-13 01:24:24 +0100

    refactroing : delete dependency

--- src/app/habit-board/habit-board.component.ts
+++ src/app/habit-board/habit-board.component.ts
@@ -1,11 +1,5 @@
 import { Component } from '@angular/core';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatTableModule } from '@angular/material/table';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSelectModule } from '@angular/material/select';
-import { MatCardModule } from '@angular/material/card';
-import {MatChipsModule} from '@angular/material/chips';
-import {MatFormFieldModule} from '@angular/material/form-field';
 import { HabitBoardCellComponent } from "./habit-board-cell/habit-board-cell.component";
 
 export interface Month {
@@ -24,8 +18,7 @@ const ELEMENT_DATA: Month[] = [
 
 @Component({
   selector: 'app-habit-board',
-  imports: [MatSlideToggleModule, MatTableModule, MatButtonModule,
-    MatSelectModule, MatCardModule, MatChipsModule, MatFormFieldModule, HabitBoardCellComponent],
+  imports: [MatTableModule, HabitBoardCellComponent],
   templateUrl: './habit-board.component.html',
   styleUrl: './habit-board.component.css'
 })
